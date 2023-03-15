e1cb7a0bf315c809a128e79eb8ae7ea66c3e4328
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-03-15 13:53:26 +0900

    WK02 - 이분탐색/분할정복/스택/큐/우선순위큐 -20230315

---
+++ WK02=이분탐색_분할정복_스택_큐_우선순위큐/문제플이_2/탑_BOJ2493.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tower_list = list(map(int, input().split()))
+tower_stack = []
+result = [0 for _ in range(n)]
+for index, tower in enumerate(tower_list):
+    if not tower_stack:
+        tower_stack.append([tower, index])
+        result[index] = 0
+        continue
+
+    while tower_stack[-1][0] <= tower :
+        tower_stack.pop()
+        if not tower_stack:
+            break
+
+    if tower_stack:
+        result[index] = tower_stack[-1][1] + 1
+    else :
+        result[index] = 0
+    tower_stack.append([tower, index])
+
+for answer in result:
+    print(answer)
