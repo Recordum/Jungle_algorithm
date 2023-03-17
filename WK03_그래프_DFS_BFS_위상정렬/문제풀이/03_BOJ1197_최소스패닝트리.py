1d677ce3caf965971270d30858972de345ab306e
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-03-17 14:44:12 +0900

    WK03-그래프탐색/DFS/BFS/위상정렬 - 20230317

--- WK03_그래프_DFS_BFS_위상정렬/문제풀이/03_BOJ1197_최소스패닝트리.py
+++ WK03_그래프_DFS_BFS_위상정렬/문제풀이/03_BOJ1197_최소스패닝트리.py
@@ -1,22 +1,32 @@
 import sys
 input = sys.stdin.readline
 
+def find_root(node):
+    if node != root[node]:
+        root[node] = find_root(root[node])
+    return root[node]
+
+def mst_kruskal():
+    global result
+    for edge in edges:
+        cost, node_a, node_b = edge
+        root_a = find_root(node_a)
+        root_b = find_root(node_b)
+        if root_a != root_b:
+            if root_a < root_b:
+                root[root_b] = root_a
+            else :
+                root[root_a] = root_b
+            result += cost
+    return
+result = 0
 v, n = map(int, input().split())
-node = [[] for _ in range(v+1)]
-result = sys.maxsize
+edges= []
+root = [i for i in range(v+1)]
 for _ in range(n):
     a, b, c = map(int, input().split())
-    node[a].append([b,c])
-    node[b].append([a,c])
-sum = 0
-connect_list = []
-for i in range(len(node)):
-    if len(node[i]) == v-1:
-        connect_list.append(node[i])
-        for i in range(len(connect_list[-1])):
-            sum += connect_list[-1][i][1]
-        result = min(result,sum)
-        sum = 0
-
+    edges.append([c, a, b])
+edges.sort()
+mst_kruskal()
 print(result)
 
