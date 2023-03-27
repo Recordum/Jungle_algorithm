a787bf32fcfd74cffe53e18539ae0a545ec2a50c
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-03-27 16:14:59 +0900

    WK04-DP/Greedy -2023-03-27

--- WK04_DP_Greedy/문제풀이/08_BOJ2098_외판원순회.py
+++ WK04_DP_Greedy/문제풀이/08_BOJ2098_외판원순회.py
@@ -1,14 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def dfs_traval():
-    for i in range(n):
-        for j in range(n):
-            cost_map[i][j]
-    return
+def dfs_traval(current, visited):
+    start_city = 0
+    if visited == (1 << n) -1:
+        if not cost_map[current][start_city]:
+            return INF
+        return cost_map[current][start_city]
+    if dp[current][visited] != INF:
+        return dp[current][visited]
+    for i in range(1,n):
+        if not visited&(1<<i) and cost_map[current][i]:
+            dp[current][visited] = min(dp[current][visited], dfs_traval(i, visited|(1<<i)) + cost_map[current][i])
+    return dp[current][visited]
 
 n = int(input())
 INF = int(1e9)
-dp = [[2] * (1 << n) for _ in range(n)]
+dp = [[INF] * (1 << n) for _ in range(n)]
 cost_map = [list(map(int,input().split())) for _ in range(n)]
-print(dp)
+print(dfs_traval(0,1))
