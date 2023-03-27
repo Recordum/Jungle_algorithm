c1792592e1d21c1bab0c457e3dfbf931b9ac8b26
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-03-27 17:34:46 +0900

    WK04-DP/Greedy -2023-03-27

--- WK04_DP_Greedy/문제풀이/09_BOJ2253_점프.py
+++ WK04_DP_Greedy/문제풀이/09_BOJ2253_점프.py
@@ -7,36 +7,31 @@ input = sys.stdin.readline
 def bfs():
     global result
     q = deque([])
-    dp = {}
-    q.append((1, 1, 2))
+    q.append((0, 0, 1))
     while q:
         count, jump, locate = q.popleft()
-        if locate == n:
-            result = min(result, count)
-            return
-        if locate > n:
-            continue
         for i in range(3):
-            next_locate = locate + jump + d_jump[i]
             if jump + d_jump[i] <= 0:
-                continue
+                break
+            next_locate = locate + jump + d_jump[i]
+            if next_locate == n:
+                result = min(result, count + 1)
+                return
             if next_locate > n:
                 continue
             if next_locate in too_small:
                 continue
-            if next_locate in dp:
-                if dp[next_locate] == jump + d_jump[i]:
-                    continue
-            dp[next_locate] = jump + d_jump[i]
+            if jump+d_jump[i] in dp[next_locate]:
+                continue
+            dp[next_locate].append(jump + d_jump[i])
             q.append((count + 1, jump+d_jump[i], next_locate))
     return
 
 result = 1e9
 d_jump = [1, 0, -1]
 n, m = map(int, input().split())
-too_small = {}
-for _ in range(m):
-    too_small[int(input())] = -1
+dp = [[] for _ in range(n+1)]
+too_small = set(int(input()) for _ in range(m))
 if 2 in too_small:
     print(-1)
     exit()
