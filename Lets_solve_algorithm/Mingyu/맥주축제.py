4858ef3885293233fe587039193d688f31e6ba87
Mingyu <92289805+Recordum@users.noreply.github.com>
2023-05-05 11:41:02 +0900

    let's solve PS

--- Lets_solve_algorithm/Mingyu/맥주축제.py
+++ Lets_solve_algorithm/Mingyu/맥주축제.py
@@ -5,19 +5,22 @@ input = sys.stdin.readline
 n, m, k = map(int, input().split())
 
 beer_list = [list(map(int, input().split())) for _ in range(k)]
-alcohol = sorted(beer_list, key= lambda x : (x[1]))
+alcohol = sorted(beer_list, key= lambda x : (x[1],x[0]))
 
+total = 0
 preference = []
 alcohol_min = 0
 for i in range(k):
     if len(preference) >= n:
-        heapq.heappop(preference)
+        sub = heapq.heappop(preference)
+        total -= sub
     heapq.heappush(preference, alcohol[i][0])
+    total += alcohol[i][0]
     alcohol_min = alcohol[i][1]
-    if sum(preference) >= m:
+    if total >= m:
          break
 
-if sum(preference) < m:
+if total < m:
     print(-1)
     exit(0)
 
---
+++ Lets_solve_algorithm/Mingyu/튀김소보루.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+
+m = int(input())
+people = [int(input()) for _ in range(m)]
+current_time = 0
+count = 0
+
+while True:
+    for i in range(len(people)):
+        if current_time % people[i] == 0:
+            count += 1
+            if count >= (n-s):
+                print(i+1)
+                exit(0)
+    current_time += 1
